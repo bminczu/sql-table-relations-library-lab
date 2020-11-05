8fa97ca95cf0461ab27bc8b8bb1e1cdbf7ae6c0e
bminczu <bminczu@gmail.com>
2020-11-05 15:26:23 -0600

    commit tree 4228711e02b6e481c71ca51ca08efc94a2f8b45c

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Series1", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Series2", 2, 2);
+
+INSERT INTO subgenre (name) VALUES ("genre1");
+INSERT INTO subgenre (name) VALUES ("genre2");
+
+INSERT INTO authors (name) VALUES ("dom");
+INSERT INTO authors (name) VALUES ("bart");
